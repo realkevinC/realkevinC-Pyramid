4f6c3345bb28b027f412572e4a352de7f1265c6f
Kevin Chen <kevinchen1688@yahoo.com>
2023-03-06 15:54:07 +0000

    done

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder pyramid = new StringBuilder();
+        
+        for (int i = 0; i < n; i++ ){
+            for(int j = 0; j <= i; j++){
+                pyramid.append("*");
+            }
+            pyramid.append("\n");
+        }
+        return pyramid.toString();
     }
 }
